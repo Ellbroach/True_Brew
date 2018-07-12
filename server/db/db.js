1756dbf79fb1a0e0d549d8d254ee08ad1f386912
Elliot Broach <Ellbroach@gmail.com>
2018-07-12 17:06:39 -0400

    travis: activate deployment

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,8 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-
+console.log('HERE', process.env.DATABASE_URL)
 const db = new Sequelize(
-  console.log('HERE', process.env.DATABASE_URL),
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false
